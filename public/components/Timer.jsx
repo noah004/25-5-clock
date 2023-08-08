3f414c717a4885ad60be9a2497e0a7b7f81e845c
noah hassan <noahhassan@gmail.com>
2023-08-08 00:43:28 -0400

    Added three states (flows): work, short, long

--- public/components/Timer.jsx
+++ public/components/Timer.jsx
@@ -4,10 +4,19 @@ import "../styles/Timer.css";
 const Timer = () => {
   const [time, setTime] = useState(0);
   const [isActive, setIsActive] = useState(false);
-  const [inputTime, setInputTime] = useState();
+  const [inputTime, setInputTime] = useState({
+    work: undefined,
+    short: 5 * 60,
+    long: 25 * 60,
+  });
   const [isPaused, setIsPaused] = useState(false);
+  const [flow, setFlow] = useState("work");
 
   useEffect(() => {
+    if (!isActive && !isPaused) {
+      setTime(inputTime[flow]);
+    }
+
     let interval;
 
     if (isActive && time > 0) {
@@ -19,11 +28,24 @@ const Timer = () => {
     }
 
     return () => clearInterval(interval);
-  }, [isActive, time]);
+  }, [isActive, time, inputTime, flow, isPaused]);
+
+  const renderFlow = (flow) => {
+    switch (flow) {
+      case "work":
+        return "Work";
+      case "short":
+        return "Short Break";
+      case "long":
+        return "Long Break";
+      default:
+        return;
+    }
+  };
 
   const handleStart = () => {
-    if (inputTime > 0) {
-      if (!isPaused) setTime(inputTime);
+    if (inputTime[flow] > 0) {
+      if (!isPaused) setTime(inputTime[flow]);
       setIsActive(true);
       setIsPaused(false);
     }
@@ -37,29 +59,58 @@ const Timer = () => {
   const handleReset = () => {
     setIsActive(false);
     setIsPaused(false);
-    setTime(0);
-    setInputTime();
+    setTime(inputTime[flow]);
+  };
+
+  const handleFlow = (newFlow) => {
+    if (flow != newFlow) {
+      setFlow(newFlow);
+      handleReset();
+    }
   };
 
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const formattedMinutes = minutes.toString().padStart(2, "0");
-    const remainingSeconds = seconds % 60;
-    const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
-    return `${formattedMinutes}:${formattedSeconds}`;
+    if (seconds >= 0) {
+      const minutes = Math.floor(seconds / 60);
+      const formattedMinutes = minutes.toString().padStart(2, "0");
+      const remainingSeconds = seconds % 60;
+      const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+      return `${formattedMinutes}:${formattedSeconds}`;
+    } else {
+      return "00:00";
+    }
   };
 
   return (
     <div className="timer">
+      <div className="flow-selector">
+        <button onClick={() => handleFlow("work")} className="btn btn-primary">
+          Work
+        </button>
+        <button onClick={() => handleFlow("short")} className="btn btn-primary">
+          Short Break
+        </button>
+        <button onClick={() => handleFlow("long")} className="btn btn-primary">
+          Long Break
+        </button>
+      </div>
       <div className="timer-display">{formatTime(time)}</div>
+      <h5 className="current-flow">{renderFlow(flow)}</h5>
+      <button className="btn btn-primary toggle-flow">Auto Switch</button>
       <div className="timer-controls">
         {!isActive && !isPaused ? (
           <>
             <input
               type="number"
               placeholder="Minutes"
-              value={Math.floor(inputTime / 60)}
-              onChange={(e) => setInputTime(60 * parseInt(e.target.value, 10))}
+              value={Math.floor(inputTime[flow] / 60)}
+              onChange={(e) => {
+                if (e.target.value >= 0)
+                  setInputTime({
+                    ...inputTime,
+                    [flow]: 60 * parseInt(e.target.value, 10),
+                  });
+              }}
             />
             <button onClick={handleStart}>Start</button>
           </>
